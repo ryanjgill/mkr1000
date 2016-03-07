b0be68212684a6441b8e0d4f2f5819c28db18a8d
ryanjgill <ryanjgill2@gmail.com>
2016-03-06 18:41:36 -0600

    moved client actions to their own file

---
+++ public/javascripts/toggleLed.js
@@ -0,0 +1,25 @@
+$(function () {
+    var socket = io.connect('http://192.168.1.249:3000')
+    socket.on('led:change', function (data) {
+        console.log(data)
+    })
+
+    socket.on('chart:data', function (data) {
+        console.log(data)
+    })
+
+    $('#on').on('click', function () {
+        socket.emit('led:on')
+        console.log('clicked on')
+    })
+
+    $('#off').on('click', function () {
+        socket.emit('led:off')
+        console.log('clicked off')
+    })
+
+    $('#pulse').on('click', function () {
+        socket.emit('led:pulse')
+        console.log('clicked pulse')
+    })
+})
